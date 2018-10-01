2d5cd056ae415418838efc2e26383bc01f616d9d
kejinzhu <1210822196@qq.com>
2018-10-01 14:27:29 +0800

    删除所有文件

--- 函数防抖.js
+++
@@ -1,9 +0,0 @@
-function debounce(fn, interval = 300) {
-    let timeout = null;
-    return function() {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            fn.apply(this, arguments);
-        }, interval);
-    };
-}
